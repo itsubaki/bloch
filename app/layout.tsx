fb819c6ca88b9045a73338be730177955a3390f1
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-11-25 10:53:58 +0900

    Update some files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from 'next'
-
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import './globals.css'
-
 import { Analytics } from '@vercel/analytics/next';
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Bloch',
@@ -30,13 +28,10 @@ html {
         `}</style>
       </head>
       <body>{children}</body>
+
       <Analytics />
-      {process.env.NEXT_PUBLIC_GTM_ID && (
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
-      )}
-      {process.env.NEXT_PUBLIC_GA_ID && (
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
-      )}
+      {process.env.NEXT_PUBLIC_GTM_ID && (<GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />)}
+      {process.env.NEXT_PUBLIC_GA_ID && (<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />)}
     </html>
   )
 }
