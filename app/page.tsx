b6ed9d98d7db1299cadca48b832d200c45455211
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-03-17 15:10:50 +0900

    Fix view reset

--- app/page.tsx
+++ app/page.tsx
@@ -347,7 +347,7 @@ export default function Bloch() {
 
       renderer.dispose()
     }
-  }, [isDarkMode, quantumState])
+  }, [isDarkMode])
 
   useEffect(() => {
     if (vectorRef.current && sceneRef.current) {
