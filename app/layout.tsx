8f2703df13b669419cc650c71c036bcfd62d4ff0
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-20 14:39:34 +0900

    Update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,23 @@ import './globals.css'
 export const metadata: Metadata = {
     title: 'Bloch Sphere',
     description: 'The geometric representation of quantum states and operations',
+    icons: {
+        icon: [
+            {
+                url: '/icon-light-32x32.png',
+                media: '(prefers-color-scheme: light)',
+            },
+            {
+                url: '/icon-dark-32x32.png',
+                media: '(prefers-color-scheme: dark)',
+            },
+            {
+                url: '/icon.svg',
+                type: 'image/svg+xml',
+            },
+        ],
+        apple: '/apple-icon.png',
+    },
 }
 
 export const viewport: Viewport = {
